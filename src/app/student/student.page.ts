a3a998279d6a36d266542b5c59e8b8cd0670b36d
AnshuEss <Anshu_Verma@essglobal.com>
2023-07-03 15:40:30 +0530

    done

--- src/app/student/student.page.ts
+++ src/app/student/student.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../api/storage.service';
 import { Router } from '@angular/router';
+import { UserService } from '../api/user.service';
+import { TosterService } from '../api/toster.service';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 @Component({
   selector: 'app-student',
   templateUrl: './student.page.html',
@@ -8,12 +12,30 @@ import { Router } from '@angular/router';
 })
 export class StudentPage implements OnInit {
   student: any;
+  TopicList:any[]=[];
+  isModalOpen:boolean=false;
+  topicId:any;
+  teachId:any;
+  topicName:any;
   constructor(
     private storage: StorageService,
-    private router: Router) { }
+    private router: Router,
+    private userService:UserService,
+    private toster:TosterService) { }
 
   async ngOnInit() {
+   this.student=await this.storage.get('student');
+   this.userService.getStuTopic({ file_no: this.student?.file_no }).subscribe((res: any) => {
+    if (res.status == 200) {
+      this.TopicList = res.data;
+    }else if(res.status==500){
+      
+    }
+  })
+  }
 
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
   }
 
   async logout(){
@@ -21,4 +43,27 @@ export class StudentPage implements OnInit {
     this.router.navigate(['']);
   }
 
+  testpaper(){
+    this.isModalOpen=true;
+  }
+
+  OpenTestPaper(item:any){
+    console.log('item',item);
+    this.isModalOpen=false;
+    this.topicId = item?.topic_id;
+    this.teachId=item?.user_id;
+    this.topicName=item?.topic
+    let obj={
+      topic_id:this.topicId,
+      file_no:this.student?.file_no,
+    }
+    this.userService.chktodayTestPaper(obj).subscribe((res: any) => {
+      if(res.status==200){
+       this.toster.error(res.msg);
+      }else if(res.status==500){
+        this.storage.set('topic_id',this.topicId);
+        this.router.navigate(['student/test-paper'])
+      }
+    });
+  }
 }
--- src/app/student/test-paper/test-paper.component.ts
+++ src/app/student/test-paper/test-paper.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild,ElementRef } from '@angular/core';
 import { UserService } from '../../api/user.service';
 import { Router } from '@angular/router';
 import { TosterService } from 'src/app/api/toster.service';
+import { Swiper } from 'swiper';
+import { register } from 'swiper/element/bundle';
+import { StorageService } from 'src/app/api/storage.service';
+register();
 @Component({
   selector: 'app-test-paper',
   templateUrl: './test-paper.component.html',
@@ -9,16 +13,48 @@ import { TosterService } from 'src/app/api/toster.service';
 })
 export class TestPaperComponent implements OnInit {
   TopicList: any[] = [];
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined;
+  swiper?: Swiper;
+  topicId:any;
+  isModalOpen=false;
+  QuestionList:any[]=[];
+  NumOfQuestion:any;
   constructor(
     private service: UserService,
     private router: Router,
-    private toster: TosterService
+    private toster: TosterService,
+    private storage:StorageService
   ) {
+    this.isModalOpen=false;
   }
 
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.topicId=await this.storage.get('topic_id');
+    console.log(this.topicId);
+    this.service.getQuesByTopicId({ topic_id: this.topicId}).subscribe((res: any) => {
+      if (res.status == 200) {
+        this.QuestionList = res.data;
+        this.NumOfQuestion = this.QuestionList.length;
+      }
+    });
   }
+
+  swiperSlideChanged(e: any) {
+   
+  }
+ 
+  swiperReady() {
+    this.swiper = this.swiperRef?.nativeElement.swiper;
+  }
+
+  chk(id:any, opt:any) {
+    this.swiper?.slideNext();
+    
+  }
+
+
 
 
 

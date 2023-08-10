98f6fbeda40fd4b27d82628b71820065e9514628
anshuverma-sdm <anshu.verma@smartdatainc.net>
2023-08-10 16:02:18 +0530

    done

--- src/app/student/time-line/time-line.component.ts
+++ src/app/student/time-line/time-line.component.ts
@@ -8,19 +8,194 @@ import { CrmService } from 'src/app/api/crm.service';
 })
 export class TimeLineComponent  implements OnInit {
 student:any
+timeLineList:any[]=[];
   constructor(
     private storage:StorageService,
     private crm:CrmService) { }
 
  async ngOnInit() {
   this.student=await this.storage.get('student');
-  this.crm.timeline({file_no:this.student?.file_no}).subscribe((res:any)=>{
-     if(res.status==200){
-
-     }else{
-      
-     }
-  })
+  if(this.student?.main_status=='NP'){
+      this.timeLineList=[
+        {status:'NP',color:'green'},
+        {status:'OP',color:'red'},
+        {status:'OR',color:'red'},
+        {status:'FAIR',color:'red'},
+        {status:'FAR',color:'red'},
+        {status:'CoeP',color:'red'},
+        {status:'CoeR',color:'red'},
+        {status:'F@H',color:'red'},
+        {status:'AIP',color:'red'},
+        {status:'DRP',color:'red'},
+        {status:'V-R',color:'red'},
+        {status:'V-G',color:'red'},
+      ]
+  }else if(this.student?.main_status=='OP'){
+    this.timeLineList=[
+      {status:'NP',color:'green'},
+      {status:'OP',color:'green'},
+      {status:'OR',color:'red'},
+      {status:'FAIR',color:'red'},
+      {status:'FAR',color:'red'},
+      {status:'CoeP',color:'red'},
+      {status:'CoeR',color:'red'},
+      {status:'F@H',color:'red'},
+      {status:'AIP',color:'red'},
+      {status:'DRP',color:'red'},
+      {status:'V-R',color:'red'},
+      {status:'V-G',color:'red'},
+    ]
+  }else if(this.student?.main_status=='OR'){
+    this.timeLineList=[
+      {status:'NP',color:'green'},
+      {status:'OP',color:'green'},
+      {status:'OR',color:'green'},
+      {status:'FAIR',color:'red'},
+      {status:'FAR',color:'red'},
+      {status:'CoeP',color:'red'},
+      {status:'CoeR',color:'red'},
+      {status:'F@H',color:'red'},
+      {status:'AIP',color:'red'},
+      {status:'DRP',color:'red'},
+      {status:'V-R',color:'red'},
+      {status:'V-G',color:'red'},
+    ]
+  }else if(this.student?.main_status=='FAIR'){
+    this.timeLineList=[
+      {status:'NP',color:'green'},
+      {status:'OP',color:'green'},
+      {status:'OR',color:'green'},
+      {status:'FAIR',color:'green'},
+      {status:'FAR',color:'red'},
+      {status:'CoeP',color:'red'},
+      {status:'CoeR',color:'red'},
+      {status:'F@H',color:'red'},
+      {status:'AIP',color:'red'},
+      {status:'DRP',color:'red'},
+      {status:'V-R',color:'red'},
+      {status:'V-G',color:'red'},
+    ]
+  }else if(this.student?.main_status=='FAR'){
+    this.timeLineList=[
+      {status:'NP',color:'green'},
+      {status:'OP',color:'green'},
+      {status:'OR',color:'green'},
+      {status:'FAIR',color:'green'},
+      {status:'FAR',color:'green'},
+      {status:'CoeP',color:'red'},
+      {status:'CoeR',color:'red'},
+      {status:'F@H',color:'red'},
+      {status:'AIP',color:'red'},
+      {status:'DRP',color:'red'},
+      {status:'V-R',color:'red'},
+      {status:'V-G',color:'red'},
+    ]
+  }else if(this.student?.main_status=='CoeP'){
+    this.timeLineList=[
+      {status:'NP',color:'green'},
+      {status:'OP',color:'green'},
+      {status:'OR',color:'green'},
+      {status:'FAIR',color:'green'},
+      {status:'FAR',color:'green'},
+      {status:'CoeP',color:'green'},
+      {status:'CoeR',color:'red'},
+      {status:'F@H',color:'red'},
+      {status:'AIP',color:'red'},
+      {status:'DRP',color:'red'},
+      {status:'V-R',color:'red'},
+      {status:'V-G',color:'red'},
+    ]
+  }else if(this.student?.main_status=='CoeR'){
+    this.timeLineList=[
+      {status:'NP',color:'green'},
+      {status:'OP',color:'green'},
+      {status:'OR',color:'green'},
+      {status:'FAIR',color:'green'},
+      {status:'FAR',color:'green'},
+      {status:'CoeP',color:'green'},
+      {status:'CoeR',color:'green'},
+      {status:'F@H',color:'red'},
+      {status:'AIP',color:'red'},
+      {status:'DRP',color:'red'},
+      {status:'V-R',color:'red'},
+      {status:'V-G',color:'red'},
+    ]
+  }else if(this.student?.main_status=='F@H'){
+    this.timeLineList=[
+      {status:'NP',color:'green'},
+      {status:'OP',color:'green'},
+      {status:'OR',color:'green'},
+      {status:'FAIR',color:'green'},
+      {status:'FAR',color:'green'},
+      {status:'CoeP',color:'green'},
+      {status:'CoeR',color:'green'},
+      {status:'F@H',color:'green'},
+      {status:'AIP',color:'red'},
+      {status:'DRP',color:'red'},
+      {status:'V-R',color:'red'},
+      {status:'V-G',color:'red'},
+    ]
+  }else if(this.student?.main_status=='AIP'){
+    this.timeLineList=[
+      {status:'NP',color:'green'},
+      {status:'OP',color:'green'},
+      {status:'OR',color:'green'},
+      {status:'FAIR',color:'green'},
+      {status:'FAR',color:'green'},
+      {status:'CoeP',color:'green'},
+      {status:'CoeR',color:'green'},
+      {status:'F@H',color:'green'},
+      {status:'AIP',color:'green'},
+      {status:'DRP',color:'red'},
+      {status:'V-R',color:'red'},
+      {status:'V-G',color:'red'},
+    ]
+  }else if(this.student?.main_status=='DRP'){
+    this.timeLineList=[
+      {status:'NP',color:'green'},
+      {status:'OP',color:'green'},
+      {status:'OR',color:'green'},
+      {status:'FAIR',color:'green'},
+      {status:'FAR',color:'green'},
+      {status:'CoeP',color:'green'},
+      {status:'CoeR',color:'green'},
+      {status:'F@H',color:'green'},
+      {status:'AIP',color:'green'},
+      {status:'DRP',color:'green'},
+      {status:'V-R',color:'red'},
+      {status:'V-G',color:'red'},
+    ]
+  }else if(this.student?.main_status=='V-R'){
+    this.timeLineList=[
+      {status:'NP',color:'green'},
+      {status:'OP',color:'green'},
+      {status:'OR',color:'green'},
+      {status:'FAIR',color:'green'},
+      {status:'FAR',color:'green'},
+      {status:'CoeP',color:'green'},
+      {status:'CoeR',color:'green'},
+      {status:'F@H',color:'green'},
+      {status:'AIP',color:'green'},
+      {status:'DRP',color:'green'},
+      {status:'V-R',color:'green'},
+      {status:'V-G',color:'red'},
+    ]
+  }else if(this.student?.main_status=='V-G'){
+    this.timeLineList=[
+      {status:'NP',color:'green'},
+      {status:'OP',color:'green'},
+      {status:'OR',color:'green'},
+      {status:'FAIR',color:'green'},
+      {status:'FAR',color:'green'},
+      {status:'CoeP',color:'green'},
+      {status:'CoeR',color:'green'},
+      {status:'F@H',color:'green'},
+      {status:'AIP',color:'green'},
+      {status:'DRP',color:'green'},
+      {status:'V-R',color:'green'},
+      {status:'V-G',color:'green'},
+    ]
+  }
  }
 
 }

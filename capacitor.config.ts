4ba6dcd5420c458e890d51a2e49d5f5c8e292a79
AnshuEss <Anshu_Verma@essglobal.com>
2023-08-10 16:04:17 +0530

    done

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,37 +1,17 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ess.global',
+  appId: 'com.ess.global',
   appName: 'Essglobal',
   webDir: 'www',
-  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https'
+  },
   "plugins": {
     "PushNotifications": {
       "presentationOptions": ["badge", "sound", "alert"]
-    },
-    "SplashScreen": {
-      "launchShowDuration": 3000,
-      //"launchAutoHide": true,
-      "launchFadeOutDuration": 3000,
-      "backgroundColor": "#ffffff",
-       "androidSplashResourceName": "splash",
-      //"androidScaleType": "CENTER_CROP",
-      "showSpinner": true,
-      "androidSpinnerStyle": "large",
-      "iosSpinnerStyle": "small",
-      "spinnerColor": "#999999",
-      "splashFullScreen": true,
-      "splashImmersive": true,
-      "layoutName": "launch_screen",
-      "useDialog": true
-    },
-  },
-  server: {
-    androidScheme: 'https',
-  },
-  // android: {
-  //   allowMixedContent:true
-  // }
+    }
+  }
 };
 
 export default config;
